f92b9c6d4a62edffdc32d7ff441f3a4b4e219662
Nazmican Dikmen <ncandikmen@outlook.com>
2024-10-21 02:21:29 +0300

    privateKey fix

--- functions/src/config/firebase.ts
+++ functions/src/config/firebase.ts
@@ -3,11 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const { privateKey } = JSON.parse(process.env.PRIVATE_KEY || "");
+
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.PROJECT_ID,
     clientEmail: process.env.CLIENT_EMAIL,
-    privateKey: process.env.PRIVATE_KEY,
+    privateKey,
   }),
 });
 
