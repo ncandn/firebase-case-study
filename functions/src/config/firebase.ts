f93c5434df7d60345f41c645204ebcbf060e7c05
Nazmican Dikmen <ncandikmen@outlook.com>
2024-10-21 03:04:47 +0300

    YML PEM Fix

--- functions/src/config/firebase.ts
+++ functions/src/config/firebase.ts
@@ -3,13 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const {privateKey} = JSON.parse(process.env.PRIVATE_KEY?.replace(/\\n/g, "\n") || "");
-
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.PROJECT_ID,
     clientEmail: process.env.CLIENT_EMAIL,
-    privateKey,
+    privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
   }),
 });
 
